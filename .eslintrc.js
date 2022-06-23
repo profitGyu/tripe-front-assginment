07be06446dfc4d4962a953470aaa34030cc6b067
silent10z <dkgueing@gmail.com>
2022-06-24 02:14:26 +0900

    chore: 린터, 프리터 초기 설정

---
+++ .eslintrc.js
@@ -0,0 +1,19 @@
+const createConfig = require('@titicaca/eslint-config-triple/create-config')
+
+const { extends: extendConfigs, overrides } = createConfig({
+  type: 'frontend',
+  project: './tsconfig.json',
+})
+
+module.exports = {
+  extends: [...extendConfigs],
+  overrides: [...overrides],
+  rules: {
+    'prettier/prettier': [
+      'error',
+      {
+        endOfLine: 'auto',
+      },
+    ],
+  },
+}
--- src/index.tsx
+++ src/index.tsx
@@ -1,19 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from 'react'
+import ReactDOM from 'react-dom/client'
 
-const root = ReactDOM.createRoot(
-  document.getElementById('root') as HTMLElement
-);
+import './index.css'
+import App from './App'
+/*
+ * NOTE: 왜 설치하면 바로 되야되는거 아니야?
+ * ? 안먹히네
+ */
+const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement)
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+)
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
